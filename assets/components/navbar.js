160e8199b46d44e1f4b9e403a8e462ad46e08be4
MacBook <71572963+guoyu24@users.noreply.github.com>
2022-08-23 20:04:56 +0100

    Removed blog, changed projects to my stuff

--- assets/components/navbar.js
+++ assets/components/navbar.js
@@ -5,31 +5,13 @@ class Navbar extends HTMLElement {
 
   connectedCallback() {
     const path = this.getAttribute("path");
+    const text = this.getAttribute("text");
+
     this.innerHTML = `
 <nav class="navbar">
-    <ul class="navbar__menu">
-      <li>
-        <a class="navbar__menu__item ${
-          path === "index" ? "navbar__menu__item--selected" : ""
-        }"
-          href="index.html"
-          >Home</a>
-      </li>
-      <li>
-        <a class="navbar__menu__item ${
-          path === "projects" ? "navbar__menu__item--selected" : ""
-        }"    
-        href="projects.html"
-        >Projects</a>
-      </li>
-      <li>
-        <a class="navbar__menu__item ${
-          path === "blog" ? "navbar__menu__item--selected" : ""
-        }" 
-        href="blog.html"
-        >Blog</a>
-      </li>
-    </ul>
+  <a class="navbar__item"    
+  href="${path}"
+  >${text}</a>
 </nav>
     `;
   }
